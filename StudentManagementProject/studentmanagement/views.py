64af002c3b2be39729bbbe7e06925b4355567eac
pradeep khadka <pradeepkhadka505@gmail.com>
2023-04-07 14:58:21 +0545

    some essential folder like template, stati and media are creaded and they are assigen in setting file ate project level

---
+++ StudentManagementProject/studentmanagement/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def employee(request):
+    return HttpResponse("My name is employee!!!!!!!! ")

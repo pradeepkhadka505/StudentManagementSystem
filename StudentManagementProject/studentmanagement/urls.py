b5a4d16b12da38da52eae85eabb1cf014b04cab3
pradeep khadka <pradeepkhadka505@gmail.com>
2023-04-10 18:23:18 +0545

    create page has created

--- StudentManagementProject/studentmanagement/urls.py
+++ StudentManagementProject/studentmanagement/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     # path('', views.employee, name='index')
-    path('student/', views.student, name='student')
+    path('student/', views.student, name='student'),
+    path('student/create', views.student_create, name='create')
 ]
--- StudentManagementProject/studentmanagement/views.py
+++ StudentManagementProject/studentmanagement/views.py
@@ -6,3 +6,6 @@ from django.shortcuts import render
 #let's create new model views
 def student(request):
     return render(request, "student/index.html")
+
+def student_create(request):
+    return render(request, "student/create.html")

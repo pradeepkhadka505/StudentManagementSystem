8d86ba598be18f0bf90bce1a5abddf4763ee16ea
pradeep khadka <pradeepkhadka505@gmail.com>
2023-04-10 10:12:57 +0545

    student template created

--- StudentManagementProject/studentmanagement/urls.py
+++ StudentManagementProject/studentmanagement/urls.py
@@ -1,8 +1,8 @@
 
-
 from django.urls import path 
 from . import views
 
 urlpatterns = [
-    path('', views.employee, name='index')
+    # path('', views.employee, name='index')
+    path('student', views.student, name='student')
 ]
--- StudentManagementProject/studentmanagement/views.py
+++ StudentManagementProject/studentmanagement/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# 
 
 # Create your views here.
 
-def employee(request):
-    return HttpResponse("My name is employee!!!!!!!! ")
+#let's create new model views
+def student(request):
+    return render(request, "student/index.html")

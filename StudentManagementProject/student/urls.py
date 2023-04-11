3cbb171c50b84840118138fd6d93303626775f24
pradeep khadka <pradeepkhadka505@gmail.com>
2023-04-11 19:57:29 +0545

    final phase of studentManagement System

--- StudentManagementProject/student/urls.py
+++ StudentManagementProject/student/urls.py
@@ -6,5 +6,8 @@ from . import views
 urlpatterns = [
     # path('', views.employee, name='index')
     path('student/', views.student, name='student'),
-    path('student/create', views.student_create, name='create')
+    path('student/create/', views.student_create, name='create'),
+    path('student/update-student/<int:roll>', views.update_student, name='update_student'),
+     path('student/do-update-student/<int:roll>', views.do_update_student, name='do_update_student'),
+    path('student/delete-student/<int:roll>', views.delete_student, name='delete_student'),
 ]
--- StudentManagementProject/student/views.py
+++ StudentManagementProject/student/views.py
@@ -1,9 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Student 
 
 # Create your views here.
 #let's create new model views
 def student(request):
-    return render(request, "student/index.html")
+    std = Student.objects.all()
+    return render(request, "student/index.html", {'std' : std})
 
 def student_create(request):
+    if request.method == 'POST':
+        print("Added")
+
+            #retrive all user inputs
+        stds_roll = request.POST.get('std_roll')
+        stds_name = request.POST.get('std_name')
+        stds_address= request.POST.get('std_address')
+        stds_phone = request.POST.get('std_phone')
+        stds_email = request.POST.get('std_email')
+
+        #create and object for models class
+
+        s = Student()  #objects for models 
+        s.roll = stds_roll 
+        s.name = stds_name 
+        s.address = stds_address
+        s.phone = stds_phone
+        s.email = stds_email
+        s.save()
+        return redirect('/student/')
     return render(request, "student/create.html")
+
+def delete_student(request, roll):
+    s = Student.objects.get(pk = roll )
+    s.delete()
+    return redirect('/student/')
+
+def update_student(request, roll):
+    std = Student.objects.get(pk = roll )
+    return render(request, "student/update.html", {'std': std})
+
+def do_update_student(request, roll):
+            #retrive all user inputs
+    stds_roll = request.POST.get('std_roll')
+    stds_name = request.POST.get('std_name')
+    stds_address= request.POST.get('std_address')
+    stds_phone = request.POST.get('std_phone')
+    stds_email = request.POST.get('std_email')
+
+    std = Student.objects.get(pk = roll )
+    std.roll = stds_roll
+    std.name = stds_name
+    std.address = stds_address
+    std.phone = stds_phone
+    std.email = stds_email
+
+    std.save()
+    return redirect('/student/')
+
+    # return render(request, "student/update.html", {'std': std})
